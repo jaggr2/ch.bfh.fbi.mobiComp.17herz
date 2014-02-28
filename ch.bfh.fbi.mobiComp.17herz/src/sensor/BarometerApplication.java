b3e50d736945d4b88af229948a857b7235dcb156
Roger Jaggi <roger.jaggi@gmail.com>
2014-02-28 11:41:01 +0100

    Signed-off-by: Roger Jaggi <roger.jaggi@gmail.com>

---
+++ ch.bfh.fbi.mobiComp.17herz/src/sensor/BarometerApplication.java
@@ -0,0 +1,93 @@
+package sensor;
+
+import ch.quantasy.tinkerforge.tinker.agent.implementation.TinkerforgeStackAgent;
+import ch.quantasy.tinkerforge.tinker.application.implementation.AbstractTinkerforgeApplication;
+import ch.quantasy.tinkerforge.tinker.core.implementation.TinkerforgeDevice;
+import com.tinkerforge.BrickletBarometer;
+import com.tinkerforge.BrickletBarometer.AirPressureListener;
+import com.tinkerforge.BrickletBarometer.AltitudeListener;
+import com.tinkerforge.Device;
+import com.tinkerforge.TinkerforgeException;
+
+/**
+ * This class is responsible for receiving, processing and delegating data about
+ * the ambient-temperature and Object-IR-temperature
+ * 
+ * @author reto
+ * 
+ */
+public class BarometerApplication extends AbstractTinkerforgeApplication
+		implements AirPressureListener, AltitudeListener {
+
+	public BarometerApplication() {
+
+	}
+
+	@Override
+	public void deviceDisconnected(
+			final TinkerforgeStackAgent tinkerforgeStackAgent,
+			final Device device) {
+		if (TinkerforgeDevice.getDevice(device) == TinkerforgeDevice.Barometer) {
+			final BrickletBarometer tempIR = (BrickletBarometer) device;
+			tempIR.removeAirPressureListener(this);
+			tempIR.removeAltitudeListener(this);
+
+			try {
+				tempIR.setAirPressureCallbackPeriod(500);
+				tempIR.setAltitudeCallbackPeriod(500);
+			} catch (final TinkerforgeException ex) {
+			}
+
+		}
+
+	}
+
+	@Override
+	public void deviceConnected(
+			final TinkerforgeStackAgent tinkerforgeStackAgent,
+			final Device device) {
+		if (TinkerforgeDevice.getDevice(device) == TinkerforgeDevice.Barometer) {
+            final BrickletBarometer tempIR = (BrickletBarometer) device;
+            tempIR.removeAirPressureListener(this);
+            tempIR.removeAltitudeListener(this);
+
+            try {
+                tempIR.setAirPressureCallbackPeriod(500);
+                tempIR.setAltitudeCallbackPeriod(500);
+            } catch (final TinkerforgeException ex) {
+            }
+
+		}
+	}
+
+    @Override
+    public void airPressure(int i) {
+        System.out.println("Air Pressure: " + i);
+
+    }
+
+    @Override
+    public void altitude(int i) {
+        System.out.println("Altitude: " + i);
+    }
+
+	@Override
+	public int hashCode() {
+		return 0;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+		final BarometerApplication other = (BarometerApplication) obj;
+		return this == other;
+	}
+}
