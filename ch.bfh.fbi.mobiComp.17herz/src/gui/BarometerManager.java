edf6453fa635b9a7c09565f0bcd75010f131fac9
Roger Jaggi <roger.jaggi@blastbeatsyndicate.com>
2014-03-21 09:40:19 +0100

    imported GUI from Reto

---
+++ ch.bfh.fbi.mobiComp.17herz/src/gui/BarometerManager.java
@@ -0,0 +1,29 @@
+package gui;
+
+import gui.view.GUIApplication;
+import ch.quantasy.tinkerforge.tinker.agency.implementation.TinkerforgeStackAgency;
+import ch.quantasy.tinkerforge.tinker.agent.implementation.TinkerforgeStackAgent;
+import ch.quantasy.tinkerforge.tinker.agent.implementation.TinkerforgeStackAgentIdentifier;
+
+public class BarometerManager {
+	public static void main(final String[] args) throws Exception {
+		final TinkerforgeStackAgentIdentifier identifier = new TinkerforgeStackAgentIdentifier(
+				"localhost");
+		final TinkerforgeStackAgent agent1 = TinkerforgeStackAgency
+				.getInstance().getStackAgent(identifier);
+
+		new Thread() {
+			@Override
+			public void run() {
+				javafx.application.Application.launch(GUIApplication.class,
+						args);
+			}
+		}.start();
+		final BarometerApplication application = new BarometerApplication();
+		agent1.addApplication(application);
+		System.in.read();
+		agent1.removeApplication(application);
+		GUIApplication.finish();
+
+	}
+}
